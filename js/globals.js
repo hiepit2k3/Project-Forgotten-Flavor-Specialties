fa37dd3de23cb69017bbbf6156be597add8ca941
Pham Song Hiep <hiepps@qualitia.com>
2024-07-24 12:52:26 +0700

    update

--- js/globals.js
+++ js/globals.js
@@ -1,2 +1,2 @@
-window.domain_backend = 'http://localhost:5000/api'
+window.domain_backend = 'https://shop-i74ykiu2x-pham-song-hieps-projects.vercel.app/api'
 window.domain_frontend = 'http://localhost:5500/'
