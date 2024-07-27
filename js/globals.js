b337747dba5e464c2e7b6e3d61b877d47ac81980
phamsonghiep <phamsonghiepb3k46@gmail.com>
2024-07-27 19:02:46 +0700

    update file globals.js, index.html and file cart.html

--- js/globals.js
+++ js/globals.js
@@ -1,3 +1,3 @@
-// window.domain_backend = 'https://shop-orcin-two.vercel.app/api'
-window.domain_backend = 'http://127.0.0.1:5000/api'
+window.domain_backend = 'https://shop-orcin-two.vercel.app/api'
+// window.domain_backend = 'http://127.0.0.1:5000/api'
 window.domain_frontend = 'dacsanquehuong.netlify.app'
